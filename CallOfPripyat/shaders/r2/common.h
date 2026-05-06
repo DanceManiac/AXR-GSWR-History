7d77b15638cd07f9db3905cfa1bfa6a2782b27a9
Dance Maniac <gazelev6@yandex.ru>
2026-05-06 19:21:43 +0300

    CoP: GSWR 1.5.21

--- CallOfPripyat/shaders/r2/common.h
+++ CallOfPripyat/shaders/r2/common.h
@@ -31,7 +31,7 @@
 // #define USE_TDETAIL                	//- shader defined
 // #define USE_LM_HEMI                	//- shader defined
 // #define USE_DISTORT                	//- shader defined
-// #define USE_SUNMASK                		//- shader defined
+ #define USE_SUNMASK                		//- shader defined
 // #define DBG_TMAPPING
 //////////////////////////////////////////////////////////////////////////////////////////
 #ifndef SMAP_size
@@ -39,6 +39,7 @@
 #endif
 #define PARALLAX_H 0.02
 #define parallax float2(PARALLAX_H, -PARALLAX_H/2)
+#define SKY_DEPTH	float(10000.f)
 
 #ifdef        USE_R2_STATIC_SUN
 #  define xmaterial half(1.0h/4.h)
@@ -46,6 +47,10 @@
 #  define xmaterial half(L_material.w)
 #endif
 //////////////////////////////////////////////////////////////////////////////////////////
+uniform half4 				 sky_color; // .xyz - sky color, .w - sky rotation
+uniform half4 				 rain_params; // .x - rain density, .y - wetness accumulator, .zw = 0
+uniform half4 				 lowland_fog_params; // x - low fog height, y - low fog density, z - base height, w - null
+uniform float4				 screen_res;
 uniform half4                hemi_cube_pos_faces;
 uniform half4                hemi_cube_neg_faces;
 uniform half4                L_material;                            // 0,0,0,mid
@@ -55,6 +60,14 @@ uniform half4                Ldynamic_dir;                        // dynamic lig
 
 uniform half4                J_direct        [6];
 uniform half4                J_spot                [6];
+
+// Глобальные параметры шейдеров --#SM+#--
+uniform	half4				m_hud_params;	//
+uniform	half4				m_blender_mode;	// 
+
+uniform half4				debug; // x - ao debug, yzw - null
+
+uniform half4				aref_params; // x - aref strength, yzw - null
 
 half          calc_fogging               (half4 w_pos)      { return dot(w_pos,fog_plane);         }
 half2         calc_detail                (half3 w_pos)      {
@@ -210,6 +223,11 @@ uniform sampler2D       s_dn_g;                	//
 uniform sampler2D       s_dn_b;                	//
 uniform sampler2D       s_dn_a;                	//
 
+uniform sampler2D 		s_dp_r;                	//
+uniform sampler2D 		s_dp_g;                	//
+uniform sampler2D 		s_dp_b;                	//
+uniform sampler2D 		s_dp_a;                	//
+
 //////////////////////////////////////////////////////////////////////////////////////////
 // Lighting/shadowing phase                     //
 uniform sampler2D       s_depth;                //
@@ -229,7 +247,7 @@ uniform sampler2D       s_tonemap;              // actually MidleGray / exp(Lw +
 //////////////////////////////////////////////////////////////////////////////////////////
 // Defines                                		//
 #define def_gloss       half(2.f /255.f)
-#define def_aref        half(200.f/255.f)
+#define def_aref        (aref_params.x)
 #define def_dbumph      half(0.333f)
 #define def_virtualh    half(0.05f)              // 5cm
 #define def_distort     half(0.05f)             // we get -0.5 .. 0.5 range, this is -512 .. 512 for 1024, so scale it
@@ -275,21 +293,36 @@ float3	v_hemi        	(float3 n)                        	{        return L_hemi_
 float3	v_hemi_wrap     (float3 n, float w)                	{        return L_hemi_color*(w + (1-w)*n.y);                   }
 float3	v_sun           (float3 n)                        	{        return L_sun_color*dot(n,-L_sun_dir_w);                }
 float3	v_sun_wrap      (float3 n, float w)                	{        return L_sun_color*(w+(1-w)*dot(n,-L_sun_dir_w));      }
-half3   p_hemi          (float2 tc)                         {
-//        half3        	t_lmh         = tex2D             	(s_hemi, tc);
-//        return  dot     (t_lmh,1.h/4.h);
-        half4        	t_lmh         = tex2D             	(s_hemi, tc);
-        return t_lmh.a;
+
+half3   p_hemi(float2 tc)
+{
+	half4 t_lmh = tex2D(s_hemi, tc);
+		
+#ifdef USE_SHOC_MODE
+	half r_lmh = (1.0/3.0);
+	return dot(t_lmh.rgb, float3(r_lmh, r_lmh, r_lmh));
+#else // USE_SHOC_MODE
+	return t_lmh.a;
+#endif // USE_SHOC_MODE
 }
 
 half   get_hemi( half4 lmh)
 {
+#ifdef USE_SHOC_MODE
+	half r_lmh = (1.0/3.0);
+	return dot(lmh.rgb, half3(r_lmh, r_lmh, r_lmh));
+#else // USE_SHOC_MODE
 	return lmh.a;
+#endif // USE_SHOC_MODE
 }
 
 half   get_sun( half4 lmh)
 {
+#ifdef USE_SHOC_MODE
+	return lmh.a;
+#else // USE_SHOC_MODE
 	return lmh.g;
+#endif // USE_SHOC_MODE
 }
 
 //	contrast function
@@ -301,6 +334,28 @@ half Contrast(half Input, half ContrastPower)
      half Output = 0.5*pow(ToRaise, ContrastPower);
      Output = IsAboveHalf ? 1-Output : Output;
      return Output;
+}
+
+// Активен-ли двойной рендер --#SM+#--
+inline bool isSecondVPActive()
+{
+	return (m_blender_mode.z == 1.f);
+}
+
+// Активен ли дебаггер АО
+inline bool ao_debug()
+{
+	return (debug.x == 1.f);
+}
+
+float rand(float n)
+{
+    return frac(cos(n)*343.42);
+}
+
+float noise(float2 tc)
+{
+    return frac(sin(dot(tc, float2(12.0, 78.0) + (timers.x) )) * 43758.0)*0.25f; 
 }
 
 #define FXPS technique _render{pass _code{PixelShader=compile ps_3_0 main();}}
